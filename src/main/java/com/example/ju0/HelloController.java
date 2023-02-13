aaf186dcd366090917361e4e0ecb5b164c13c527
lakkulakku <61740467+lakkulakku@users.noreply.github.com>
2023-02-13 10:56:52 +0900

    Source code update

--- src/main/java/com/example/ju0/HelloController.java
+++ src/main/java/com/example/ju0/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.ju0;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	
 	@GetMapping
 	public String hello() {
-		return "Hello World! Application is running";
+		return "hello";
 	}
 }
---
+++ src/main/java/com/example/ju0/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.ju0;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringDockerApplication.class);
+	}
+
+}
