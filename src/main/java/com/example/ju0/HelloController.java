8f74604685a6a898d82d05c96d9477915180a7d3
lakkulakku <61740467+lakkulakku@users.noreply.github.com>
2023-02-14 09:30:52 +0900

    Source code update

--- src/main/java/com/example/ju0/HelloController.java
+++ src/main/java/com/example/ju0/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.ju0;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 	
-	@GetMapping
+	@RequestMapping("/")
 	public String hello() {
-		return "hello";
+		return "Hello! Application is Running";
 	}
 }
